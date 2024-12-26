a376bb880ad64f29ca851aee779b8eb8dc3af23c
Godhanded <aezeigwe2003@gmail.com>
2024-12-26 12:41:01 +0100

    fix: include timestamp in tracked tokens for alerts when volume exceeds threshold

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import os
 from typing import Any
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
+from datetime import datetime
 from bot.storage import save_token_data, load_token_data
 from bot.telegram_bot import send_server_telegram_alert
 
@@ -33,10 +34,10 @@ def process_pool():
         "token1_volume": token_details[1]["tokenAmount"],
         "time_stamp": pool_data[0]["timestamp"],
     }
-    tracked_tokens[pool_data[0]["signature"]] = new_pool
-    # if new_pool["token0_volume"] > 1000 or new_pool["token1_volume"] > 1000:
-    save_token_data(tracked_tokens)
-    send_server_telegram_alert(pool_data[0]["signature"], new_pool)
+    if new_pool["token0_volume"] > 1000 or new_pool["token1_volume"] > 1000:
+        tracked_tokens[pool_data[0]["signature"]] = {**new_pool,"time_stamp":datetime.fromtimestamp(pool_data[0]["timestamp"])}
+        save_token_data(tracked_tokens)
+        send_server_telegram_alert(pool_data[0]["signature"], new_pool)
 
     return jsonify({"status": "success"}),200
 
